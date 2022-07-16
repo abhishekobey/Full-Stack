c7c726a11de3e21a3249c907d58cf945221d7db0
Abhishek obey <abhishek@surveyheart.com>
2022-07-16 19:35:20 +0530

    :zap: deploy to heroku

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import bodyParser from "body-parser";
 import contactRouters from "./routes/contactRouters.js";
+import path from "path";
+import morgan from "morgan";
 dotenv.config();
 
 //connect to db
@@ -11,6 +13,20 @@ mongoose.connect(
     process.env.DB_CONNECT,
     {useNewUrlParser: true}
 );
+if(process.env.NODE_ENV === 'developement') {
+    app.use(morgan('dev'))
+}
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
+    })
+}
+
 const db = mongoose.connection
 db.on('error', (err) => console.error(err))
 db.once('open', () => console.log("CONNECTED"))
